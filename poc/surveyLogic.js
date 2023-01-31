4e4d04645c4ccccbb92e4b495ff00696ae38205a
yaelBrown <yaelrbrown@gmail.com>
2023-01-30 23:31:45 -0600

    save: saved survey logic concept

---
+++ poc/surveyLogic.js
@@ -0,0 +1,44 @@
+const data = {
+  questions: [
+    {
+      question_id: 1,
+      question: "Whats your favorite color?",
+      question_order: 0,
+    },
+    {
+      question_id: 2,
+      question: "Whats your favorite food?",
+      question_order: 1,
+    },
+    {
+      question_id: 3,
+      question: "Whats your favorite number?",
+      question_order: 2,
+    },
+    {
+      question_id: 4,
+      question: "Where were you born?",
+      question_order: 3,
+    },
+    {
+      question_id: 5,
+      question: "What is your favorite city to visit?",
+      question_order: 4,
+    },
+    {
+      question_id: 6,
+      question: "When is your birthday?",
+      question_order: 5,
+    },
+  ],
+  questions_length: 6,
+  responses: [
+    {
+      response_id: 5,
+      response_question_id: "1",
+      response: "test",
+      responded_date: "2023-01-27T00:39:12.000Z",
+    },
+  ],
+  responses_length: 1,
+};
